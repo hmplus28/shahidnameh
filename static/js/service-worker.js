372387be39624d3341e6c49e4b36146cfd1125e5
Z User <z@container>
2026-09-04 10:08:32 +0000

    feat: جایگزینی ایموجی/کاراکترهای یونیکد با آیکون‌های SVG ایرانی
    - تقویت icon.svg و icon-maskable.svg با لاله قرمز، هلال و تذهیب اسلامی
    - ساخت ۲۸ آیکون SVG جدید در static/icons/:
      * tulip (لاله شهادت)، crescent-star (هلال و ستاره)
      * mosque (گنبد مسجد)، kafiyeh (چفیه فلسطینی)
      * medal (مدال افتخار با لاله)، testament (وصیت‌نامه با خوشنویسی)
      * red-crescent (هلال احمر ایران)، star-islamic (ستاره هشت‌پر)
      * home, search, offline, menu, install, play, external
      * arrow-left, arrow-right, page, plus, clock, users, dashboard
      * logout, close, mihrab, empty, default-avatar, city-silhouette
    - ساخت templates/icons/icon.html به‌عنوان جزء Django برای inline SVG با ۳۰ موتیف
    - جایگزینی کامل ایموجی‌ها در ۱۴ قالب:
      * base.html (ناوبری پایین، آیکون نصب، برند، لینک ادمین)
      * martyrs/home.html (آیکون‌های ارزش، جداکننده، لاله آمار، لاله پرتره)
      * martyrs/martyr_directory.html (هلال و ستاره، ستاره‌های تزئینی)
      * martyrs/martyr_detail.html (علامت مجروحیت با هلال احمر، آیکون پخش، حالت خالی)
      * martyrs/partials/martyr_results.html (آواتار پیش‌فرض، آیکون جستجو، پیکان)
      * offline.html (آیکون سیگنال و وای‌فای با موتیف ایرانی)
      * panel/base_panel.html (آیکون‌های پیشخوان، لیست، ساعت، کاربران، خروج)
      * پنل‌های dashboard/martyr_list/martyr_form/user_form/user_list/login
    - به‌روزرسانی CSS:
      * site.css: استایل‌های جدید برای .icn، .value-icon، .empty-symbol،
        .portrait-tulip، .hero-decor-star، .page-hero-icon، .section-divider
        + استایل‌های SVG برای آیکون‌های inline در قالب‌ها
      * panel.css: استایل flex برای آیکون‌های ناوبری، دکمه‌ها و لینک‌ها
    - به‌روزرسانی service-worker.js: v6 → v7، اضافه‌شدن همه SVGهای جدید به APP_SHELL
    - به‌روزرسانی manifest.webmanifest: آیکون‌های شورتکات با search و dashboard
    - default-avatar.png → default-avatar.svg (سرباز با چفیه و لاله روی پیشانی)
    - city-silhouette.png → city-silhouette.svg (یادمان شهدا با گنبد، برج و لاله)
    
    همه SVGها با موتیف‌های مرتبط با شهدا ساخته شده‌اند:
    - لاله قرمز (نماد اصلی شهادت)
    - چفیه با پترن کاری شبکه‌ای
    - گنبد مسجد با مناره و هلال
    - مدال افتخار با روبان و لاله
    - وصیت‌نامه با خوشنویسی و مهر
    - هلال احمر ایران (جای صلیب سفید)
    - ستاره هشت‌پر اسلامی با لاله مرکزی
    - سیلوئت یادمان شهدا با برج، گنبد و لاله‌های صحرایی

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -1,5 +1,25 @@
-const CACHE_NAME = 'shahidnameh-v6';
-const APP_SHELL = ['/', '/martyrs/', '/offline/', '/static/css/site.css', '/static/css/fonts.css', '/static/fonts/vazirmatn-arabic.woff2', '/static/fonts/vazirmatn-latin.woff2', '/static/fonts/vazirmatn-latin-ext.woff2', '/static/js/site.js', '/static/manifest.webmanifest', '/static/icons/icon.svg', '/static/icons/icon-maskable.svg', '/static/images/hero-banner.jpg', '/static/images/memories-bg.jpg', '/static/images/offline-bg.jpg', '/static/images/directory-hero.jpg', '/static/images/front-bg.jpg', '/static/images/testament-bg.jpg', '/static/images/city-silhouette.png', '/static/images/default-avatar.png'];
+const CACHE_NAME = 'shahidnameh-v7';
+const APP_SHELL = [
+  '/', '/martyrs/', '/offline/',
+  '/static/css/site.css', '/static/css/fonts.css',
+  '/static/fonts/vazirmatn-arabic.woff2', '/static/fonts/vazirmatn-latin.woff2', '/static/fonts/vazirmatn-latin-ext.woff2',
+  '/static/js/site.js', '/static/manifest.webmanifest',
+  '/static/icons/icon.svg', '/static/icons/icon-maskable.svg',
+  '/static/icons/tulip.svg', '/static/icons/crescent-star.svg', '/static/icons/mosque.svg',
+  '/static/icons/kafiyeh.svg', '/static/icons/medal.svg', '/static/icons/testament.svg',
+  '/static/icons/red-crescent.svg', '/static/icons/star-islamic.svg',
+  '/static/icons/home.svg', '/static/icons/search.svg', '/static/icons/offline.svg',
+  '/static/icons/menu.svg', '/static/icons/install.svg', '/static/icons/play.svg',
+  '/static/icons/external.svg', '/static/icons/arrow-left.svg', '/static/icons/arrow-right.svg',
+  '/static/icons/page.svg', '/static/icons/plus.svg', '/static/icons/clock.svg',
+  '/static/icons/users.svg', '/static/icons/dashboard.svg', '/static/icons/logout.svg',
+  '/static/icons/close.svg', '/static/icons/mihrab.svg', '/static/icons/empty.svg',
+  '/static/icons/default-avatar.svg', '/static/icons/city-silhouette.svg',
+  '/static/images/hero-banner.jpg', '/static/images/memories-bg.jpg',
+  '/static/images/offline-bg.jpg', '/static/images/directory-hero.jpg',
+  '/static/images/front-bg.jpg', '/static/images/testament-bg.jpg',
+  '/static/images/city-silhouette.png', '/static/images/default-avatar.png'
+];
 
 const isCacheable = (request, response) => request.method === 'GET' && response && response.ok && new URL(request.url).origin === self.location.origin;
 const putInCache = async (request, response) => {
@@ -10,7 +30,7 @@ const putInCache = async (request, response) => {
 };
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL).catch((err) => console.warn('Cache addAll partial fail:', err))).then(() => self.skipWaiting()));
 });
 
 self.addEventListener('activate', (event) => {
